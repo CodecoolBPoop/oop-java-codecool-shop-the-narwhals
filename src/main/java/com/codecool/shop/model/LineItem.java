c592a3065f01106c5f818dca63d1b3bcdd599f0c
doraKoreny <dorakoreny@gmail.com>
2019-01-09 11:30:40 +0100

    implemented Order and LineItem classes

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -1,4 +1,36 @@
 package com.codecool.shop.model;
 
-public class LineItem {
+public class LineItem extends BaseModel {
+    private int productId;
+    private int numberOfProducts;
+    private float totalPrice;
+
+    public LineItem(Product product, String name, String description) {
+        super(name, description);
+        numberOfProducts = 1;
+        this.productId = product.getId();
+        this.totalPrice = product.getDefaultPrice();
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void add(Product product) {
+        numberOfProducts ++;
+        totalPrice += product.getDefaultPrice();
+    }
+
+    public void remove(Product product) {
+        numberOfProducts --;
+        totalPrice -= product.getDefaultPrice();
+    }
+
+    public boolean isEmpty() {
+        if (numberOfProducts < 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,4 +1,55 @@
 package com.codecool.shop.model;
 
-public class Order {
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
+public class Order extends BaseModel{
+    private int id;
+    private List<LineItem> items = new ArrayList<>();
+    private int totalSum;
+
+    private final String CURRENCY = "Credits";
+    private Currency defaultCurrency;
+
+    public Order(Product product, String orderName, String orderDescription) {
+        super(orderName, orderDescription);
+        setDefaultCurrency(Currency.getInstance(CURRENCY));
+        addProduct(product);
+        totalSum += product.getDefaultPrice();
+    }
+
+    public void setDefaultCurrency(Currency defaultCurrency) {
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public void addProduct(Product product) {
+        LineItem lineItem = getLineItemByProductId(product.getId());
+        if (lineItem == null) {
+            String lineItemName = "Line item " + product.getName();
+            String lineItemDescription = "Some " + product.getName() + "s";
+            LineItem newLineItem = new LineItem(product, lineItemName, lineItemDescription);
+            items.add(newLineItem);
+        } else {
+            lineItem.add(product);
+        }
+        totalSum += product.getDefaultPrice();
+    }
+
+    public void removeProduct(Product product) {
+        LineItem lineItem = getLineItemByProductId(product.getId());
+        lineItem.remove(product);
+        totalSum -= product.getDefaultPrice();
+        if (lineItem.isEmpty()) {
+            items.remove(lineItem);
+        }
+    }
+
+    private LineItem getLineItemByProductId(int productId) {
+        return items.stream()
+                                    .filter(item -> item.getProductId() == productId)
+                                    .findFirst()
+                                    .orElse(null);
+    }
 }

7b56d540c1562c03654ac08a5fc45205d378a6c5
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-01-22 14:54:11 +0100

    created Address class

---
+++ src/main/java/com/codecool/shop/util/Address.java
@@ -0,0 +1,15 @@
+package com.codecool.shop.util;
+
+public class Address {
+    private String address;
+    private String city;
+    private String country;
+    private String zipCode;
+
+    public Address(String address, String city, String country, String zipCode) {
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.zipCode = zipCode;
+    }
+}

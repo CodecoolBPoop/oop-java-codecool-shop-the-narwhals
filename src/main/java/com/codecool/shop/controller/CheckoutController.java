4ff89a1cb9f9b3faae007cac5ff9fa2ef657403e
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-01-22 11:46:31 +0100

    created checkout page with form

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.WebConnection;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("checkout.html", context, resp.getWriter());
+    }
+}

1c32ce7e6c64365be417db8e5ffc30f377c49f00
Krisztina Nagy <nakuaat@gmail.com>
2019-01-23 15:40:17 +0100

    formatted input fields

--- src/main/webapp/static/scripts/payment.js
+++ src/main/webapp/static/scripts/payment.js
@@ -1,6 +1,13 @@
 payment = {
-
-    document.querySelector('#card').addEventListener('keydown', (e) => {
-        e.target.value = e.target.value.replace(/(\d{4})(\d+)/g, '$1 $2')
-    })
+    cardNumberModifier: function(){
+        // document.querySelector('#card').addEventListener('keydown', (e) => {
+        //     e.target.value = e.target.value.replace(/(\d{4})(\d+)/g, '$1 $2')
+        // })
+        $('#card').keyup(function() {
+            $(this).val($(this).val().replace(/(\d{4})(\d+)/g, '$1-$2'))
+        });
+        console.log("Something")
+    }
 };
+
+payment.cardNumberModifier();
